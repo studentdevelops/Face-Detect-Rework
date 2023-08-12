bd069aac2228946b84308440953b3eed4b6425d9
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2023-08-12 11:04:45 +0530

    changing router.replace back to router.push and reporting this bug

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,77 +1,110 @@
-'use client'
+"use client";
 
-import Link from "next/link"
-import { useRouter } from 'next/navigation'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 import { motion } from "framer-motion";
 
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-import styles from "./login.module.css"
+import styles from "./login.module.css";
 import { validateEmail } from "@/utils/validators";
 import { useUserContext } from "../context/contextStore";
 
-
 export default function Login() {
-  const router = useRouter()
-  const { user, setUser, count, setCount } = useUserContext()
+    const router = useRouter();
+    const { user, setUser, count, setCount } = useUserContext();
 
-  const onSuccessfulLogin = async () => {
-    const response = await fetch("/api/fetchdetails")
-    const result = await response.json()
-    setUser(result.name);
-    setCount(result.count);
-  }
+    const onSuccessfulLogin = async () => {
+        const response = await fetch("/api/fetchdetails");
+        const result = await response.json();
+        setUser(result.name);
+        setCount(result.count);
+    };
 
-  const LoginSubmit = async (e) => {
-    e.preventDefault();
+    const LoginSubmit = async (e) => {
+        e.preventDefault();
 
-    const LoginForm = document.forms.namedItem("LoginForm");
-    const data = new FormData(LoginForm);
-    const email = data.get("email");
-    const password = data.get("password");
-    if (validateEmail(email)) {
-      const response = await toast.promise(
-        fetch("/api/login", {
-          method: "POST",
-          body: JSON.stringify({ email, password })
-        }),
-        {
-          pending: 'Loading',
-          error: 'Error! Try Again! 🤯'
+        const LoginForm = document.forms.namedItem("LoginForm");
+        const data = new FormData(LoginForm);
+        const email = data.get("email");
+        const password = data.get("password");
+        if (validateEmail(email)) {
+            const response = await toast.promise(
+                fetch("/api/login", {
+                    method: "POST",
+                    body: JSON.stringify({ email, password }),
+                }),
+                {
+                    pending: "Loading",
+                    error: "Error! Try Again! 🤯",
+                }
+            );
+            const result = await response.json();
+            if (result?.userId) {
+                toast.success("Welcome 👌", {
+                    position: toast.POSITION.TOP_RIGHT,
+                });
+                onSuccessfulLogin();
+                router.push('/');
+            } else {
+                toast.warn("Invalid email or password", {
+                    position: toast.POSITION.TOP_RIGHT,
+                });
+            }
         }
-      );
-      const result = await response.json();
-      if (result?.userId) {
-        toast.success("Welcome 👌", { position: toast.POSITION.TOP_RIGHT });
-        onSuccessfulLogin();
-        router.replace({ href: "/"})
-      } else {
-        toast.warn("Invalid email or password", { position: toast.POSITION.TOP_RIGHT });
-      }
-    }
-  }
+    };
 
-
-  // useEffect(() => {
-    
-  // },[user])
-  
-
-  return (
-    <main className={styles.LoginMain}>
-      <div>
-
-        <form method="POST" name="LoginForm" onSubmit={LoginSubmit} className={styles.LoginForm}>
-          <input type="texts" placeholder="Email" name="email" />
-          <input type="password" placeholder="Password" name="password" />
-          <p>New here? <Link href={"/signup"}>Click Here!</Link></p>
-          <motion.button
-            whileTap={{ scale: 0.9, transition: { duration: 0.1, ease: "easeInOut" } }}
-            whileHover={{ scale: 1.1, transition: { duration: 0.2, ease: "easeInOut" } }}
-            className={styles.LoginButton}>Login</motion.button>
-        </form>
-      </div>
-    </main>
-  )
+    return (
+        <main className={styles.LoginMain}>
+            <div>
+                <form
+                    method="POST"
+                    name="LoginForm"
+                    onSubmit={LoginSubmit}
+                    className={styles.LoginForm}
+                >
+                    <input type="texts" placeholder="Email" name="email" />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                    />
+                    <p>
+                        New here? <Link href={"/signup"}>Click Here!</Link>
+                    </p>
+                    <motion.button
+                        whileTap={{
+                            scale: 0.9,
+                            transition: { duration: 0.1, ease: "easeInOut" },
+                        }}
+                        whileHover={{
+                            scale: 1.1,
+                            transition: { duration: 0.2, ease: "easeInOut" },
+                        }}
+                        className={styles.LoginButton}
+                    >
+                        Login
+                    </motion.button>
+                </form>
+                <motion.button
+                    onTap={() => {
+                      console.log("router.push('/')")
+                        router.push("/");
+                    }}
+                    whileTap={{
+                        scale: 0.9,
+                        transition: { duration: 0.1, ease: "easeInOut" },
+                    }}
+                    whileHover={{
+                        scale: 1.1,
+                        transition: { duration: 0.2, ease: "easeInOut" },
+                    }}
+                    className={styles.LoginButton}
+                >
+                    Login
+                </motion.button>
+            </div>
+        </main>
+    );
 }
